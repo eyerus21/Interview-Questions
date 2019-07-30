812c147348f42a1076f4cb732075a9d1bd58ee96
eyerus21 <50240801+eyerus21@users.noreply.github.com>
2019-07-30 13:28:28 +0300

    Update Queue with two stacks.java

--- Queue with two stacks.java
+++ Queue with two stacks.java
@@ -1,54 +1,60 @@
-import java.utill.*;
- public class QueueUsingTwoStacks{
- 	static class Queue{
- 		static Stack<Double>S1 = new Stack<Double>();
-	        static Stack<Double>S2 = new Stack<Double>();
- 		static void enqueue(int a){
+import java.util.*;
+ public class QUTS{
+ 	import java.util.*;
 
- //move all elements from s1 to s2
- 			while(!S1.isempty())
-      {
-      S2.push(s1.pop());//s1.pop
-	  }
+class quts
+{
+    static class Queue{
+        static Stack<Double>S1 = new Stack<Double>();
+        static Stack<Double>S2 = new Stack<Double>();
+        static void enqueue(double a){
+
+            //move all elements from s1 to s2
+            while(!S1.isEmpty())
+            {
+                S2.push(S1.pop());//s1.pop
+            }
 //push item into s1
-   S1.push(a);
+            S1.push(a);
 //push everything back to s1
-   while(!S2.isempty())
-   { 
-         S1.push(S2.pop());//s2.pop
+            while(!S2.isEmpty())
+            {
+                S1.push(S2.pop());//s2.pop
 
-   }
+            }
+
+        }
+        //Dequeue an item from the queue
+        static double dequeue() {
+
+            //if 1st stack is empty
+            if (S1.isEmpty())
+                {
+                    System.out.println("queue is empty");
+                    System.exit(0);
+                }
+                //return top of s1
+                double a = S1.peek();
+                S1.pop();
+                return a;
+
+
+
+        } };
+    public static void main(String[]args)
+    {
+        Queue q=new Queue();
+        q.enqueue(1.0);
+        q.enqueue(2.2);
+        q.enqueue(3.43);
+
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+        System.out.println(q.dequeue());
+
+    }
+
+
 
 }
-          //Dequeue an item from the queue
-      static double dequeue(){
-
-      //if 1st stack is empty
-      	if (S1.isempty()){
-          {
-          	System.out.println("queue is empty");
-          	System.exit(0);
-          }
-       //return top of s1
-          int a=S1.peek();
-          S1.pop();
-          return a;
-   }
- }
-
-}; 
-     public static void main(String[]args)
-{   
-	Queue q=new Queue();
-	q.enqueue(1);
-        q.enqueue(2);
-        q.enqueue(3);
-
-    System.out.println(q.enqueue());
-    System.out.println(q.enqueue());
-    System.out.println(q.enqueue());
-
-}
-
-
-}
+  
