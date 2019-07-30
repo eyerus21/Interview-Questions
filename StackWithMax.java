19f5826de8034e724aaa986c2fd158ef9f05b056
eyerus21 <50240801+eyerus21@users.noreply.github.com>
2019-07-30 13:39:41 +0300

    Update StackWithMax.java

--- StackWithMax.java
+++ StackWithMax.java
@@ -1,58 +1,50 @@
-import java.utill.*;
+import java.util.*;
 class StackWithMax{
     static class SMax{
-//main stack
-    	static Stack<Double>mainstack=new Stack<Double>();
-    	//to keep track of max element
-    	static Stack<Double>trackstack=new Stack<Double>();
+        //main stack
+        static Stack<Double>mainstack=new Stack<Double>();
+        //to keep track of max element
+        static Stack<Double>trackstack=new Stack<Double>();
 
-static void push (double x)
+        static void push (double x)
 
-{
-   mainstack.push(x);
-   if (mainstack.size()==1)
-   { 
-    trackstack,push(x);
-    return;
-  } 
+        {
+            mainstack.push(x);
+            if (mainstack.size()==1)
+            {
+                trackstack.push(x);
+                return;
+            }
 
-if(x>trackstack.peek())
-     trackstack.push(x);
-else
-	trackstack.push(trackstack.peek())
+            if(x>trackstack.peek())
+                trackstack.push(x);
+            else
+                trackstack.push(trackstack.peek());
 
-}
+        }
 
-static double getMax(){
+        static double getMax(){
 
-	return trackstack.peek();
-}
-static void pop()
-{ 
-mainstack.pop();
-trackstack.pop();
-}
+            return trackstack.peek();
+        }
+        static void pop()
+        {
+            mainstack.pop();
+            trackstack.pop();
+        }
 
-};
+    };
 
-public static void main ( String [] args)
-{
-  StackWithMax s = new StackWithMax();  
-    s.push(21);  
-    System.out.println(s.getMax());  
-    s.push(14);  
-    System.out.println(s.getMax());  
-    s.push(28);  
-    System.out.println(s.getMax());
-
-
-
-}
+    public static void main ( String [] args)
+    {
+        SMax s = new SMax();
+        s.push(21);
+        System.out.println(s.getMax());
+        s.push(14);
+        System.out.println(s.getMax());
+        s.push(28);
+        System.out.println(s.getMax());
 
 
 
-
-
-
-
-}
+    }}
